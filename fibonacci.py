c44d5bb61ee934f63baf786444ea874e654afaea
narendra100703 <96768972+narendra100703@users.noreply.github.com>
2022-05-20 18:38:49 +0530

    def feb(n):  a,b=0,1  print(a,b,end=' ')  for i in range(2,n):      c=a+b      print(c,end=' ')      a=b      b=c n=int(input()) feb(n)

--- fibonacci.py
+++ fibonacci.py
@@ -1,11 +1,10 @@
 def feb(n):
-    a,b=0,1
-    print(a,b,end=' ')
-    for i in range(2,n):
+ a,b=0,1
+ print(a,b,end=' ')
+ for i in range(2,n):
      c=a+b
      print(c,end=' ')
      a=b
      b=c
-     
 n=int(input())
 feb(n)
