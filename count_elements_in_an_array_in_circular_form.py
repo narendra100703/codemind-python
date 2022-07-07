4212e5e6b895f02b4c3ef7ae39aac39b24e0cafa
narendra100703 <96768972+narendra100703@users.noreply.github.com>
2022-07-07 15:10:43 +0530

    n = int(input()) arr = list(map(int,input().split())) count = 0 for i in range(0,n-1):     if i==0:         if (arr[i+1]%2 and arr[n-1]%2==0)or(arr[i+1]%2==0 and arr[n-1]%2):             count+=1     if i!=0:         if ((arr[i-1]%2 and arr[i+1]%2==0)or(arr[i-1]%2==0 and arr[i+1]%2)):             count+=1 if (arr[0]%2 and arr[n-2]%2==0) or (arr[0]%2==0 and arr[n-2]%2):     count+=1 print(count)

--- count_elements_in_an_array_in_circular_form.py
+++ count_elements_in_an_array_in_circular_form.py
@@ -1,11 +1,13 @@
-x = int(input())
-l = list(map(int,input().split()))
-c=0
-if (l[x-2]%2==0 and l[0]%2!=0) or (l[x-2]%2!=0 and l[0]%2==0):
-    c+=1
-if (l[1]%2==0 and l[x-1]%2!=0) or (l[1]%2!=0 and l[x-1]%2==0):
-    c+=1
-for i in range(1,len(l)-1):
-    if (l[i-1]%2==0 and l[i+1]%2!=0) or (l[i-1]%2!=0 and l[i+1]%2==0):
-        c+=1
-print(c)
+n = int(input())
+arr = list(map(int,input().split()))
+count = 0
+for i in range(0,n-1):
+    if i==0:
+        if (arr[i+1]%2 and arr[n-1]%2==0)or(arr[i+1]%2==0 and arr[n-1]%2):
+            count+=1
+    if i!=0:
+        if ((arr[i-1]%2 and arr[i+1]%2==0)or(arr[i-1]%2==0 and arr[i+1]%2)):
+            count+=1
+if (arr[0]%2 and arr[n-2]%2==0) or (arr[0]%2==0 and arr[n-2]%2):
+    count+=1
+print(count)
