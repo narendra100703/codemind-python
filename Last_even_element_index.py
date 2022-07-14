217a962ba8bc8f656938e17596a2de54af3a4ec3
narendra100703 <96768972+narendra100703@users.noreply.github.com>
2022-07-14 14:12:49 +0530

    n = int(input()) arr = list(map(int,input().split())) for i in range(n-1,-1,-1):     if arr[i]%2==0:         print(i)         break

--- Last_even_element_index.py
+++ Last_even_element_index.py
@@ -1,7 +1,6 @@
-n=int(input())
-arr=list(map(int,input().split()))
-temp=0
-for i in range(0,n):
+n = int(input())
+arr = list(map(int,input().split()))
+for i in range(n-1,-1,-1):
     if arr[i]%2==0:
-        temp=i
-print(temp)
+        print(i)
+        break
