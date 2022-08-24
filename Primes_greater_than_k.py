b91acdc78b7e9cf9baf7747f137f3df0cb31e43b
narendra100703 <96768972+narendra100703@users.noreply.github.com>
2022-08-24 12:14:40 +0530

    def isprime(n):     if n==1:         return False     else:         for i in range(2,int(n**0.5)+1):             if n%i==0:                 return False         return True n=int(input()) l=list(map(int,input().split())) k=int(input()) c=0 for i in l:     if i>=k and isprime(i):         c+=1 print(c)

--- Primes_greater_than_k.py
+++ Primes_greater_than_k.py
@@ -1,17 +1,16 @@
-def prime(n):
+def isprime(n):
     if n==1:
-        return 0
-    elif n==2:
-        return 1
-    for i in range(2,int(n**0.5)+2):
-        if n%i==0:
-            return 0
-    return 1
+        return False
+    else:
+        for i in range(2,int(n**0.5)+1):
+            if n%i==0:
+                return False
+        return True
 n=int(input())
-a=list(map(int,input().split()))
-b=int(input())
+l=list(map(int,input().split()))
+k=int(input())
 c=0
-for i in a:
-    if prime(i) and i>=b:
+for i in l:
+    if i>=k and isprime(i):
         c+=1
 print(c)
